ee63319192d4bb7276ac723b62aeef2ea645d796
cawleygl <grantlcawley@gmail.com>
2021-01-21 16:43:17 -0600

    Installed inquirer

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, process.argv[2], (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
