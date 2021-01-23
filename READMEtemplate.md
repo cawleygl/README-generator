6602ac324a0ac3b274e2571594e2c8e21723428f
cawleygl <grantlcawley@gmail.com>
2021-01-23 15:17:44 -0600

    removed node_modules

---
+++ READMEtemplate.md
@@ -0,0 +1,29 @@
+# 
+<!-- projname -->
+![GitHub license]
+<!-- license: badge -->
+
+## Description
+<!-- projDes -->
+
+## Table of Contents
+<!-- links to each section -->
+
+## Installation
+<!-- cmdInstall -->
+
+## Usage
+<!-- infoUse -->
+
+## License
+<!-- license: explains which license the application is covered under -->
+
+## Contributing
+<!-- infoCont -->
+
+## Tests
+<!-- cmdTest -->
+
+## Questions
+<!-- username: with link to github profile -->
+<!-- email: with instructions on how to reach me with additional questions -->
--- index.js
+++ index.js
@@ -3,14 +3,69 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+  type: 'input',
+  name: 'username',
+  message: 'What is your GitHub username?',
+},
+{
+  type: 'input',
+  name: 'email',
+  message: 'What is your email address?',
+},
+{
+  type: 'input',
+  name: 'projName',
+  message: "What is your project's name?",
+},
+{
+  type: 'input',
+  name: 'projDes',
+  message: 'Please provide a short description of your project.',
+},
+{
+  type: 'list',
+  name: 'license',
+  message: 'What kind of license should your project have?',
+  choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "NONE"]
+},
+{
+  type: 'input',
+  name: 'cmdInstall',
+  message: 'What command should be run to install dependencies?',
+  default: "npm i"
+},
+{
+  type: 'input',
+  name: 'cmdTest',
+  message: 'What command should be run to run tests?',
+  default: "npm test"
+},
+{
+  type: 'input',
+  name: 'infoUse',
+  message: 'What does the user need to know about using the repo?',
+},
+{
+  type: 'input',
+  name: 'infoCont',
+  message: 'What does the user need to know about contributing to the repo?',
+},
+
+];
+
+inquirer.prompt(questions).then((data) => {
+  const res = JSON.stringify(data);
+  console.log(res);
+  writeToFile(res);
+});
 
 // TODO: Create a function to write README file
 
-function writeToFile(fileName, data) {
-    fs.writeFile(`${fileName}.md`, process.argv[2], (err) =>
-        err ? console.error(err) : console.log('Success!')
-    );
+function writeToFile(data) {
+  fs.writeFile("README.md", data, (err) =>
+    err ? console.error(err) : console.log('Success!')
+  );
 
 }
 
