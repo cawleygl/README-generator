88608869bfa5ec3a23e53b65439a2d40054c639b
cawleygl <grantlcawley@gmail.com>
2021-01-23 22:05:47 -0600

    Completed function to write to README.md file with proper formatting

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ const questions = [{
   type: 'input',
   name: 'cmdInstall',
   message: 'What command should be run to install dependencies?',
-  default: "npm i"
+  default: 'npm i'
 },
 {
   type: 'input',
   name: 'cmdTest',
   message: 'What command should be run to run tests?',
-  default: "npm test"
+  default: 'npm test'
 },
 {
   type: 'input',
@@ -54,26 +54,9 @@ const questions = [{
 
 ];
 
-inquirer.prompt(questions).then((data) => {
-  const username = JSON.stringify(data.username);
-  const email = JSON.stringify(data.email);
-  const projName = JSON.stringify(data.projName);
-  const projDes = JSON.stringify(data.projDes);
-  const license = JSON.stringify(data.license);
-  const cmdInstall = JSON.stringify(data.cmdInstall);
-  const cmdTest = JSON.stringify(data.cmdTest);
-  const infoUse = JSON.stringify(data.infoUse);
-  const infoCont = JSON.stringify(data.infoCont);
-
-  const res = `Hi! My name is ${username}\n## Description`
-
-  console.log(res);
-  writeToFile(res);
-});
-
 // TODO: Create a function to write README file
 function writeToFile(data) {
-  fs.writeFile("README.md", data, (err) =>
+  fs.writeFile('README.md', data, (err) =>
     err ? console.error(err) : console.log('Success!')
   );
 
@@ -81,6 +64,36 @@ function writeToFile(data) {
 
 // TODO: Create a function to initialize app
 function init() {
+  inquirer.prompt(questions).then((data) => {
+
+  switch (data.license) {
+    case "MIT":
+      var licenseBadge = "![GitHub License](https://img.shields.io/badge/License-MIT-yellow.svg)"
+      var license = "This project is licensed under the MIT license."
+      break;
+    case "APACHE 2.0":
+      var licenseBadge = "![GitHub License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+      var license = "This project is licensed under the APACHE 2.0 license."
+      break;
+    case "GPL 3.0":
+      var licenseBadge = "![GitHub License](https://img.shields.io/badge/License-GPLv3-blue.svg)"
+      var license = "This project is licensed under the GPL 3.0 license."
+      break;
+    case "BSD 3":
+      var licenseBadge = "![GitHub License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+      var license = "This project is licensed under the BSD 3 license."
+      break;
+    default:
+      var licenseBadge = ""
+      var license = "Please inquire for further licencing information on this project."
+      break;
+  }
+
+  const res = `# ${data.projName}\n${licenseBadge}\n\n## Description\n${data.projDes}\n\n## Table of Contents\n* [Installation](#installation)\n\n* [Usage](#usage)\n\n * [License](#license)\n\n * [Contributing](#contributing)\n\n* [Tests](#tests)\n\n* [Questions](#questions)\n\n## Installation\nTo install necessary dependencies, run the following command: ${data.cmdInstall}\n\n## Usage\n${data.infoUse}\n\n## License\n${license}\n\n## Contributing\n${data.infoCont}\n\n## Tests\nTo run tests, run the following command: ${data.cmdTest}\n\n## Questions\nIf you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username}).\n`
+
+  writeToFile(res);
+
+  }).catch((err) => console.error(err));
 
 }
 
